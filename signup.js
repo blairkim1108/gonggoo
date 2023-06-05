d5f14c4800bd6290c5f5d7f52978ad72ba8ded49
Blair Kim <kimblair1108@gmail.com>
2023-06-06 00:29:35 +0900

    no message

--- signup.js
+++ signup.js
@@ -1,1 +1,16 @@
-alert("WELCOME TO 공구콘솔!");
+const signupForm = document.querySelector("#signup-form");
+const greeting = document.querySelector("#greeting"); 
+const usernameInput = document.getElementById("#name");
+
+const HIDDEN_CLASSNAME = "hidden"; 
+
+function onSignupSubmit(event) {
+    event.preventDefault(); 
+    signupForm.classList.add(HIDDEN_CLASSNAME);
+    const username = usernameInput.value;
+    greeting.innerText = `${username}님, 회원가입이 완료되었습니다.`;
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
+} 
+
+signupForm.addEventListener("submit", onSignupSubmit);
+// first argument of a function gives information about the event object that just occurred
